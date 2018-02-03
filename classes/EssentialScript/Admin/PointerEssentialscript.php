ccb430afe38dbcd14e360894d72ea1888f759a2d
Giulio <giupersu@yahoo.it>
2018-02-03 19:32:26 +0100

    Use provided system metakey `dismissed_wp_pointers` instead of custom

--- classes/EssentialScript/Admin/PointerEssentialscript.php
+++ classes/EssentialScript/Admin/PointerEssentialscript.php
@@ -20,47 +20,52 @@
 namespace EssentialScript\Admin;
 
 /**
- * Description of PointerEssentialscript
+ * Make the Pointer.
  *
  * @author docwho
  */
 class PointerEssentialscript extends \EssentialScript\Core\Pointers {
-	
+	/**
+	 * @var string To collect meta fields
+	 */
 	private $userMeta;
-	
+	/**
+	 * Setup class.
+	 */
 	public function __construct() {
 		
 		parent::__construct();
-
+		// Retrieve all meta fields
 		$this->userMeta = (string) get_user_meta(
-			get_current_user_id(), 'dismissed_escript_pointers', true );
+			get_current_user_id(), 'dismissed_wp_pointers', true );
 		// Include the pointer scripts and styles.
 		add_action( 'admin_enqueue_scripts',
 			array ( $this, 'enqueueScript' ) );
+		// Displays the HTML code
 		add_action( 'essentialscript_pointer',
 			array ( $this, 'displayPointer' ) );
+		// Writes the init JavaScript code in the footer of the page.
 		add_action( 'admin_print_footer_scripts',
 			array (	$this, 'footerScript' ) );
 	}
-	
+	/**
+	 * Displays the Pointer.
+	 */
 	public function displayPointer() {
 
 		$dismissed_pointers = explode( ',', $this->userMeta );
-		
-		if ( ! in_array( 'plugin_editor_notice', $dismissed_pointers, true ) ) :
+
+		if ( ! in_array( 'escript_editor_notice', $dismissed_pointers, true ) ) :
 			// Get a back URL
 			$referer = wp_get_referer();
-			$excluded_referer_basenames = array( 'tools.php?page=essentialscript', 'wp-login.php' );
+			$excluded_referer_basenames = array( 'tools.php', 'wp-login.php' );
 			
-			if ( $referer && ! in_array( basename( parse_url( $referer, PHP_URL_PATH ) ), $excluded_referer_basenames, true ) ) {
-				echo '---------1';
+			if ( $referer && ! in_array( basename( parse_url( $referer,
+				PHP_URL_PATH ) ), $excluded_referer_basenames, true ) ) {
 				$return_url = $referer;
 			} else {
-				echo '---------2';
 				$return_url = admin_url( '/' );
 			}
-			echo '-------------------';
-			var_dump( $return_url );
 ?>
 <div id="file-editor-warning" class="notification-dialog-wrap file-editor-warning hide-if-no-js hidden">
 	<div class="notification-dialog-background"></div>
@@ -68,7 +73,7 @@ class PointerEssentialscript extends \EssentialScript\Core\Pointers {
 		<div class="file-editor-warning-content">
 			<div class="file-editor-warning-message">
 				<h1><?php _e( 'Heads up!' ); ?></h1>
-				<p><?php _e( 'You appear to be adding JavaScript to your Wordpress platform. We recommend that you pay attention while you are about to do so. Adding JavaScript may 
+				<p><?php _e( 'You appear to be adding custom JavaScript to your Wordpress platform. We recommend that you pay attention while you are about to do so. Adding JavaScript may 
 introduce <b>dangerous code</b> that break your Web Site making it vulnerable to hacker attacks. Cross-site scripting is a type of hack that uses JavaScript.' ); ?></p>
 				<p><?php _e( 'If you are fully aware of these risks and how to avoid them, then you can proceed beyond this screen, if something goes wrong you can always disable 
 					this plugin. Otherwise stop here and go back.' ); ?></p>
@@ -83,7 +88,9 @@ introduce <b>dangerous code</b> that break your Web Site making it vulnerable to
 <?php
 endif; // editor warning notice		
 	}
-
+	/**
+	 * JavaScript code to initialize the Pointer.
+	 */
 	public function footerScript() {
 ?>
 <script type="text/javascript">
--- classes/EssentialScript/Core/Pointers.php
+++ classes/EssentialScript/Core/Pointers.php
@@ -20,37 +20,50 @@
 namespace EssentialScript\Core;
 
 /**
- * Description of Pointer
+ * Abstract class that defines the methods and data to be used and set up with
+ * the Pointer.
  *
  * @author docwho
  */
 abstract class Pointers {
-	
-	const ESSENTIALSCRIPT_DISMISSED_POINTER = 'dismissed_escript_pointers';
+	/**
+	 *  Uses provided system metakey instead of custom. It could change later. 
+	 */
+	const WORDPRESS_DISMISSED_POINTER = 'dismissed_wp_pointers';
+	//const ESSENTIALSCRIPT_DISMISSED_POINTER = 'dismissed_essentialscript_pointers';
+
+	/**
+	 * @since 0.9
+	 */
 	const ESSENTIALSCRIPT_VER = '0.9';
-	
+	/**
+	 * Setup class.
+	 */
 	public function __construct() {
-		//  WordPress global variable associated with the current user
-		global $current_user;
 		// Retrieves the information pertaining to the currently logged in user
-		get_currentuserinfo();
-		// Determine if dismissed_escript_pointers is present in wp_usermeta
+		$current_user = wp_get_current_user();
+		// Determine if dismissed_wp_pointers is present in wp_usermeta
 		$havemeta = metadata_exists( 'user', $current_user->ID,
-			self::ESSENTIALSCRIPT_DISMISSED_POINTER );
+			self::WORDPRESS_DISMISSED_POINTER );
 		// Adds meta data to a user if there is no assigned meta.
 		if ( ! $havemeta ) {
 			add_user_meta( get_current_user_id(),
-			self::ESSENTIALSCRIPT_DISMISSED_POINTER, '', false );
+			self::WORDPRESS_DISMISSED_POINTER, '', false );
 		}
 	}
 	
-	abstract public function displayPointer(); 
-	
+	abstract public function displayPointer();
+	/**
+	 * Registers every functional script for the Pointer
+	 */
 	public function enqueueScript() {
 		wp_register_script(
 			'wp-essentialscript-pointers',
 			plugins_url( 'lib/wp-essentialscript-pointers.js',
-				ESSENTIAL_SCRIPT1_PLUGIN_FILE ), array (),
+				ESSENTIAL_SCRIPT1_PLUGIN_FILE ),
+			array ( 'wp-util',	// Necessary to use wp.ajax.post
+				'wp-a11y',		// Necessary to use wp.a11y.speak
+				'wp-sanitize'),	// Necessary to use wp.sanitize
 			self::ESSENTIALSCRIPT_VER, false );
 		
 		wp_enqueue_script( 'wp-essentialscript-pointers' );
